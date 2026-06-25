a4fce253c2b27ce7fc5caa6072fb4468c4285c07
powerzyme1234 <powerzymenutrition@gmail.com>
2026-06-25 14:29:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 
 // ---- Environment variables (set these in Render, never hardcode) ----
 const {
